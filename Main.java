36d4fefe7128ecbe322667dbb45514c5036eb87c
SYED MUSA RAZA <79834398+syedmusa-eng@users.noreply.github.com>
2021-05-07 06:13:35 +0200

    Separate Test APP

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        //Calling a method to calculating area of a circle
+        System.out.println(AreaCalculator.area(5.0));
+        System.out.println(AreaCalculator.area(-1));
+
+        //Calling a method to calculate area of a rectangle
+        System.out.println(AreaCalculator.area(5.0,4.0));
+        System.out.println(AreaCalculator.area(-1.0,4.0));
+    }
+}
